d15fff701872a97272a067ec6e7926956750cdd2
steph-del <dev.stephane.delplanque@gmail.com>
2018-10-30 10:13:35 +0100

    use the new ng6.0 service injection, forgotten imports to remove

--- projects/tb-tsb-lib/src/lib/tb-tsb-lib.module.ts
+++ projects/tb-tsb-lib/src/lib/tb-tsb-lib.module.ts
@@ -4,12 +4,6 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { RepositoryService } from './_services/repository.service';
-import { BasevegRepositoryService } from './_repositories/baseveg.service';
-import { BaseflorRepositoryService } from './_repositories/baseflor.service';
-import { BdtfxRepositoryService } from './_repositories/bdtfx.service';
-import { Pvf2RepositoryService } from './_repositories/pvf2.service';
-
 import { TbTsbLibComponent } from './tb-tsb-lib.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { MatSelectModule, MatInputModule, MatAutocompleteModule, MatProgressSpinnerModule, MatTooltipModule, MatChipsModule, MatIconModule, MatButtonModule } from '@angular/material';
@@ -31,9 +25,4 @@ import { RepositoryModule } from './_repositories/repository.module';
     TbTsbLibComponent, SearchBoxComponent
   ]
 })
-export class TbTsbLibModule {
-  providers: [
-    RepositoryService, BasevegRepositoryService, BaseflorRepositoryService,
-    BdtfxRepositoryService, Pvf2RepositoryService
-  ];
-}
+export class TbTsbLibModule { }
